dc09f73c86b0009c9ff17a56cdce96a8d02bbf99
tom eliason <tom.eliason@yahoo.com>
2025-11-17 09:24:04 -0700

    2

--- 10-data.py
+++ 10-data.py
@@ -32,6 +32,13 @@ try:
         connection.cursor().execute("GRANT CREATE SESSION, RESOURCE TO sh01;")
         connection.cursor().execute("GRANT UNLIMITED TABLESPACE TO sh01;")
         connection.cursor().execute("BEGIN ORDS_ADMIN.ENABLE_SCHEMA(p_enabled => TRUE, p_schema => UPPER('sh01'), p_url_mapping_type => 'BASE_PATH', p_url_mapping_pattern => LOWER('sh01'), p_auto_rest_auth => TRUE); END;")
+        connection.cursor().execute("CREATE TABLE sh01.customers AS SELECT * FROM sh.customers;")
+        connection.cursor().execute("CREATE TABLE sh01.countries AS SELECT * FROM sh.countries;")
+        connection.cursor().execute("CREATE TABLE sh01.sales AS SELECT * FROM sh.sales;")
+        connection.cursor().execute("ALTER TABLE SH01.CUSTOMERS ADD CONSTRAINT CUSTOMERS_PK PRIMARY KEY (CUST_ID);")
+        connection.cursor().execute("ALTER TABLE SH01.SALES ADD CONSTRAINT SALES_PK PRIMARY KEY (PROD_ID, CUST_ID, TIME_ID, CHANNEL_ID, PROMO_ID);")
+        connection.cursor().execute("ALTER TABLE SH01.SALES ADD CONSTRAINT SALES_CUSTOMER_FK FOREIGN KEY (CUST_ID) REFERENCES SH01.CUSTOMERS(CUST_ID);")
+        connection.cursor().execute("CREATE TABLE SH01.PAYMENTS (   Payment_ID NUMBER PRIMARY KEY,   Customer_ID NUMBER,   Sale_ID NUMBER,   Payment_Amount NUMBER NOT NULL,   Payment_Date DATE NOT NULL,   Payment_Method VARCHAR2(50),   VISA VARCHAR2(16),   AMEX VARCHAR2(15),   DISCOVER VARCHAR2(16),   MC VARCHAR2(16), BANKING_ACCOUNT VARCHAR2(17),   ROUTING_NUMBER VARCHAR2(9),   FOREIGN KEY (Customer_ID) REFERENCES SH01.CUSTOMERS(CUST_ID));")
     except oracledb.Error as e:
         print(f"Error:  {e}")
 
